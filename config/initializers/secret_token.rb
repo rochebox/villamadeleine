862b8036703ced799f12080440fd7bd5a88577fe
rochebox <rochebox@gmail.com>
2016-09-13 01:05:12 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Villamadeleine::Application.config.secret_key_base = '4f6fd9fb73ac121f59b120f7703e41d4894be5a705cb84c452c9db1a6de4be2f8bb386d08dcba53aac6656fa24666813bd4afe44daf4633b7cb4c865be6d535a'
