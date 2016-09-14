5576fe7ded29d434364a037f5b441d0e2b110eab
rochebox <rochebox@gmail.com>
2016-09-14 03:10:46 +0000

    Adding Devise Gem l 16 nomster

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Villamadeleine::Application.routes.draw do
+  devise_for :users
   root 'static_pages#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
